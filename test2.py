aa358c89b915c74d1f6f786242c04d347709c6c6
lamaroufle <arthurdegan@hotmail.fr>
2015-12-11 19:20:10 +0100

    update

--- test2.py
+++ test2.py
@@ -5,31 +5,31 @@ import sys
 from path import path
 import cPickle
 
-NbImg=30; # Nombre d'images par folder a prendre en compte
-DbPath="./dataset/paris" # chemin database contenant les dossiers categories
-DescPath="./Desc" # chemin de sauvegarde des descripteurs
-DataPath="./Data"
+nbImg=5; # Nombre d'images par folder a prendre en compte
+dbPath="./dataset/paris" # chemin database contenant les dossiers categories
+desPath="./Desc" # chemin de sauvegarde des descripteurs
+dataPath="./Data"
 
 # On verifie si la BDD existe
-if os.path.exists(DataPath + 	'/desfinal'):
-	print('Chargement de ' + DataPath + "/desfinal")
-	dd=cPickle.load(open(DataPath + "/desfinal"))
-	print("Descriptors loaded from " + DataPath + "/desfinal")
+if os.path.exists(dataPath + '/desfinal'):
+	print('Chargement de ' + dataPath + "/desfinal")
+	desfinal=cPickle.load(open(dataPath + "/desfinal"))
+	print("Descriptors loaded from " + dataPath + "/desfinal")
 # Sinon on la cree
 else:
-	dir=os.listdir(DbPath)
+	dir=os.listdir(dbPath)
 	for i in range(0,len(dir)):
-		dire= DbPath + '/' + dir[i]
-		print("\nTraitement de "+ str(NbImg) +" images de : " + dire)
+		dire= dbPath + '/' + dir[i]
+		print("\nTraitement de "+ str(nbImg) +" images de : " + dire)
 
 		# Preparation des variables de barre de chargement
 		k=0
-		NbDesc=0
+		nbDes=0
 		# fin de pretaration
 		for f in path(dire).walkfiles():
-			if k<30:
+			if k<nbImg:
 				# On verifie que les desc de l'image f ne sont pas deja calcules
-				if os.path.exists(DescPath + '/desc-' + dir[i] + str(k+1)):
+				if os.path.exists(desPath + '/desc-' + dir[i] + str(k+1)):
 					if k==29:
 						print('Descriptors already computed.')
 						print('Jumping to next folder...')
@@ -44,16 +44,16 @@ else:
 							gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 							sift = cv2.SIFT()
 							kp,des = sift.detectAndCompute(gray,None)
-							NbDesc=NbDesc + des.shape[0]
+							nbDes=nbDes + des.shape[0]
 
 							# Incrementation et affichage chargement
 							k=k+1
-							sys.stdout.write('\r' + 'Chargement : ' + str(k) + '/' + str(NbImg) + ' Nombre de descripteurs : ' + str(NbDesc) + ' soit ' + str(NbDesc/k) + ' desc/img')
+							sys.stdout.write('\r' + 'Chargement : ' + str(k) + '/' + str(nbImg) + ' Nombre de descripteurs : ' + str(nbDes) + ' soit ' + str(nbDes/k) + ' desc/img')
 							sys.stdout.flush()
 							# Fin chargement
 
 							# Sauvegarde desc actuels
-							cPickle.dump(des, open(DescPath + "/desc-" + dir[i] + str(k), "wb"))
+							cPickle.dump(des, open(desPath + "/desc-" + dir[i] + str(k), "wb"))
 
 						except:
 							# Si l'image pose probleme, on l'ignore et on passe a la suivante.
@@ -63,29 +63,25 @@ else:
 	k=1
 	desfinal=np.array([])
 	print('\nChargement des descripteurs...')
-	NbImg=len(os.listdir(DescPath))
-	for f in path(DescPath).walkfiles():
+	nbImg=len(os.listdir(desPath))
+	for f in path(desPath).walkfiles():
 		try:
 			Des=cPickle.load(open(f,'rb'))
 			# Concatenation des descripteurs
 			desfinal = np.append(desfinal, Des)
 			desfinal = np.reshape(desfinal, (len(desfinal)/128, 128))
-			NbDes=desfinal.shape[0]
+			nbDes=desfinal.shape[0]
 		except:
 			# Erreur : on recalcule le descripteur
 			print('Le fichier ' + f + ' est invalide, il sera ignore.')
 
 		#Chargement
-		sys.stdout.write('\r' + 'Chargement : ' + str(k) + '/' + str(NbImg) + ' Nombre de descripteurs : ' + str(NbDes) + ' soit ' + str(NbDes/k) + 'desc/img')
+		sys.stdout.write('\r' + 'Chargement : ' + str(k) + '/' + str(nbImg) + ' Nombre de descripteurs : ' + str(nbDes) + ' soit ' + str(nbDes/k) + 'desc/img')
 		sys.stdout.flush()
 		k=k+1
 
 	print('\nEnregistrement de la liste des descripteurs...')
-	cPickle.dump(desfinal, open(DataPath + "/desfinal", 'wb'))
-
-# Preparation de la matrice de descripteurs pour le Kmeans
-desc = np.reshape(desfinal, (len(desfinal)/128, 128))
-desc = np.float32(desc)
+	cPickle.dump(desfinal, open(dataPath + "/desfinal", 'wb'))
 
 # Preparation critere arret kmeans : iterations et epsilon
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
@@ -95,12 +91,12 @@ flags = cv2.KMEANS_RANDOM_CENTERS
 
 # Apply KMeans
 print("\nApplying kmeans...")
-ret,labels,centers = cv2.kmeans(desc,6000,criteria,10,flags)
+ret,labels,centers = cv2.kmeans(desfinal,6000,criteria,10,flags)
 
 print('Sauvegarde de labels...')
-cPickle.dump(desfinal, open(DataPath + "/labels", 'wb'))
+cPickle.dump(desfinal, open(dataPath + "/labels", 'wb'))
 print('Sauvegarde de centers...')
-cPickle.dump(desfinal, open(DataPath + "/centers", 'wb'))
+cPickle.dump(desfinal, open(dataPath + "/centers", 'wb'))
 print('YEAH!')
 
 # Debut du SVM

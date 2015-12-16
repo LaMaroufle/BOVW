71ecf9dd2a64799abf9fd0af3c16a92043b6441e
lamaroufle <arthurdegan@hotmail.fr>
2015-12-16 18:46:24 +0100

    fonctionnel

--- hist.py
+++ hist.py
@@ -4,10 +4,12 @@ import os
 import sys
 from path import path
 import cPickle
+import matplotlib.pyplot as plt
 
 dbPath="./dataset/paris" # chemin database contenant les dossiers categories
 desPath="./Desc" # chemin de sauvegarde des descripteurs
 dataPath="./Data"
+imgPath='/louvre/paris_louvre_000032.jpg'
 
 def L1(liste1, liste2):
 	d = 0
@@ -15,29 +17,42 @@ def L1(liste1, liste2):
 		d = d + abs(liste1[i] - liste2[i])
 	return d
 
-if not os.path.exists(dataPath + '/labels') or not os.path.exists(dataPath + '/centers'):
-	print('labels ou centers manquant, lancer test3.py pour les creer')
+if not os.path.exists(dataPath + '/dist'):
+	if not os.path.exists(dataPath + '/labels') or not os.path.exists(dataPath + '/centers'):
+		print('labels ou centers manquant, lancer test3.py pour les creer')
+	else:
+		print('Chargement de centers...')
+		centers=cPickle.load(open(dataPath + "/centers", 'rb'))
+
+	img = cv2.imread(dbPath + imgPath,1)
+	gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	sift = cv2.SIFT()
+	kp,des = sift.detectAndCompute(gray,None)
+
+	dist=np.array([])
+	i=0
+	print ("\nCalcul des distances :")
+	for row in des:
+		sys.stdout.write('\r' + 'Traitement du descripteur : ' + str(i+1) + '/' + str(des.shape[0]))
+		sys.stdout.flush()
+		distImgCent=[]
+		for cent in centers:
+			distImgCent.append(sum(abs(row-cent)))
+		dist = np.append(dist, distImgCent.index(min(distImgCent)))
+		i=i+1
+
+	print('\nEnregistrement des distances...')
+	cPickle.dump(dist, open(dataPath + '/dist', "wb"))
+
 else:
-	print('Chargement de centers...')
-	centers=cPickle.load(open(dataPath + "/centers", 'rb'))
+	print('Distances trouve, chargement...')
+	dist=cPickle.load(open(dataPath + "/dist", 'rb'))
 
-img = cv2.imread(dbPath + '/notredame/paris_notredame_000998.jpg',1)
-gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-sift = cv2.SIFT()
-kp,des = sift.detectAndCompute(gray,None)
+print(dist)
+plt.hist(dist,5000)
+plt.title("Histograme de " + dbPath + imgPath)
+plt.xlabel("Classes")
+plt.ylabel("Nombre")
+plt.show()
 
-hist=np.array([])
-distImgCent=np.array([])
-i=0
-j=0
-print ("\nCalcul des distances :")
-for row in des:
-	sys.stdout.write('\r' + 'Traitement du descripteur : ' + str(i+1) + '/' + str(des.shape[0]))
-	sys.stdout.flush()
-	for cent in centers:
-		distImgCent=L1(row,cent)
-		j+1
-	i+1
-
-print(distImgCent)
 #distImgCent.index(min(distImgCent))
